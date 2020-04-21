2be82462270049b611f5ebe8ab2dd97c08cf5631
Noland Seigler <nolandseigler@gmail.com>
2020-04-20 23:05:04 -0500

    practice

--- practice.swift
+++ practice.swift
@@ -1,3 +1,16 @@
 for _ in 1...15 {
   print("Stay thirsty.")
 }
+
+//random with string interpolation and break
+var guessedNum = Int.random(in: 1...15)
+
+for counter in 1...15 {
+  // Add your code below 🤔
+  if counter == guessedNum {
+    print("It's \(guessedNum)!!")
+    break
+  }
+  print("Is it \(counter)?")
+}
+//
