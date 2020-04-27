4ecbec7b85d62ec4baf645d67dc1f92be92c9a1d
Noland Seigler <nolandseigler@gmail.com>
2020-04-26 22:08:18 -0500

    mixtape project print

--- Mixtape.swift
+++ Mixtape.swift
@@ -1,3 +1,4 @@
+// Write your code below 📼
 //Top Songs
 print(".------------------------.")
 print("| Top Songs       90 min |")
@@ -11,4 +12,21 @@ var mixtape = [String]()
 
 mixtape.append("THE SCOTTS, Kid Cudi, Travis Scott - THE SCOTTS")
 mixtape.append("Doja Cat - Say So")
-mixtape.append("")
+mixtape.append("Drake - Toosie Slide")
+mixtape.append("Juice WRLD - Righteous")
+mixtape.append("Lil Mosey - Blueberry Faygo")
+mixtape.append("BENEE, Gus Dapperton - Supalonely")
+mixtape.append("The Weeknd - Blinding Lights")
+mixtape.append("Dua Lipa - Break My Heart")
+mixtape.append("Surfaces - Sunday Best")
+mixtape.append("Roddy Ricch - The Box")
+
+print(mixtape.count)
+
+mixtape.remove(at: 4)
+mixtape.remove(at: 7)
+mixtape.insert(mixtape.remove(at: mixtape.count - 1), at: 0)
+print("All Songs")
+for song in mixtape {
+  print(song)
+}
