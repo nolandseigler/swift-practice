8593ce5af17ab66d66c28df5fbc93db93cbd6801
Noland Seigler <nolandseigler@gmail.com>
2020-04-26 21:49:36 -0500

    mixtape project

---
+++ Mixtape.swift
@@ -0,0 +1,14 @@
+//Top Songs
+print(".------------------------.")
+print("| Top Songs       90 min |")
+print("|     __  ______  __     |")
+print("|    (  )|).....|(  )    |")
+print("|    (__)|)_____|(__)    |")
+print("|    ________________    |")
+print("|___/_._o________o_._\\___|")
+
+var mixtape = [String]()
+
+mixtape.append("THE SCOTTS, Kid Cudi, Travis Scott - THE SCOTTS")
+mixtape.append("Doja Cat - Say So")
+mixtape.append("")
