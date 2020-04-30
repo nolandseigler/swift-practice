03f3bf24eac5436e26241d9815754d60a6b97980
Noland Seigler <nolandseigler@gmail.com>
2020-04-29 23:57:38 -0500

    dictionaries

--- DictionaryPractice.swift
+++ DictionaryPractice.swift
@@ -4,3 +4,8 @@ var emptyLiteral: [String: Int] = [:]
 
 var emptyInitializer = [String: Bool]()
 
+// Write your code below 👇
+
+var roleModels: [String: String] = ["Mr. Rogers": "Fred McFeely Rogers", "The Crocodile Hunter": "Stephen Robert Irwin", "Bill Nye the Science Guy": "William Sanford Nye"]
+
+print(roleModels)
