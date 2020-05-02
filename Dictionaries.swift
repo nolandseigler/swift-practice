a6d5a1ea083fffeb4d2bdd39053af9735ac90af4
Noland Seigler <nolandseigler@gmail.com>
2020-05-02 16:13:27 -0500

    dictionary practice

--- Dictionaries.swift
+++ Dictionaries.swift
@@ -24,3 +24,42 @@ abbreviations["LOL"] = "Laugh Out Loud"
 abbreviations.updateValue("Be Right Back", forKey: "BRB")
 
 print(abbreviations)
+
+var rainbowHex = [
+  "red": "#ff0000",
+  "pink": "#ffc0cb",
+  "yellow": "#ffff00",
+  "maroon": "#800000",
+  "green":  "#00ff00",
+  "blue": "#0000ff",
+  "violet": "#ee82ee"
+]
+
+//remove using nil which is Swift for null
+rainbowHex["maroon"] = nil
+print(rainbowHex)
+//remove using removeValue method
+rainbowHex.removeValue(forKey: "pink")
+print(rainbowHex)
+//remove all using removeAll method
+rainbowHex.removeAll()
+print(rainbowHex)
+
+var flowerNames = [
+  "Lily": "Lilium",
+  "Sunflower": "Helianthus",
+  "Orchid": "Orchidaceae",
+  "Daffodil": "Narcissus"
+]
+//saving values from dictionary leads to a type of Optional.
+// var sunflowerScientific = flowerNames["Sunflower"]
+// print(sunflowerScientific)
+//unwrap value from optional if you are certain the key holds a value.
+var sunflowerScientific = flowerNames["Sunflower"]!
+print(sunflowerScientific)
+
+//use if let to avoid the Optional issue and avoid errors from nil values
+
+if let lilyScientific = flowerNames["Lily"] {
+  print("A lily is referred to as a \(lilyScientific) in the science community.")
+}
