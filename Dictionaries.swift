9bf3f29109528a4701f1f67a8c104e6eb80d6a68
Noland Seigler <nolandseigler@gmail.com>
2020-05-02 17:02:17 -0500

    dictionary practice

--- Dictionaries.swift
+++ Dictionaries.swift
@@ -77,3 +77,24 @@ for (greekName, romanName) in mythology {
   print("\(greekName) is also known as \(romanName)"
   )
 }
+
+var lemonadeStand = [
+  "April": 8.50,
+  "May": 12.75,
+  "June": 22.50,
+  "July": 38.25,
+  "August": 32.50,
+  "September": 11.50
+]
+
+var total: Double = 0.0
+
+// Add your code below 🍋
+
+for monthlyProfit in lemonadeStand.values {
+  total += monthlyProfit
+}
+
+
+
+print("Total profits are \(total)")
